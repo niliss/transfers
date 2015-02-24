f1882e50abd597618f27ff0545cf8f47eab667dc
Niels Siskens <siskens@gmail.com>
2015-02-24 12:35:01 -0500

    ready for uploading

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,4 +1,5 @@
 class Transfer < ActiveRecord::Base
   belongs_to :sender
   belongs_to :receiver
+  before_safe :update_balance
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ActiveRecord::Base
+  validates :balance, :numericality => { :greater_than: 50000 }	
 end
