f6487c4a0d1aaf4256ff6f8fbcc0c2c1907bdb24
Niels Siskens <siskens@gmail.com>
2015-02-25 17:12:18 -0500

    Final solution

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,5 +1,17 @@
 class Transfer < ActiveRecord::Base
-  belongs_to :sender
-  belongs_to :receiver
-  before_safe :update_balance
+  belongs_to :sender, class_name: 'User' 
+  belongs_to :receiver, class_name: 'User' # This is to map Receiver to User
+  after_create :update_balance
+  
+  def update_balance()
+  	sender.balance = sender.balance - self.amount
+  	receiver.balance = receiver.balance + self.amount
+  	
+  	sender.save
+  	receiver.save
+  end
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  validates :balance, :numericality => { :greater_than: 50000 }	
+  validates :balance, :numericality => { greater_than: 50000 }	
+  has_many :transfers
 end
